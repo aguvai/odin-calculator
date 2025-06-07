10d1c6a7c5e06c51f0d30a5892fcef167c0cdac2
aguvai <145935152+aguvai@users.noreply.github.com>
2025-06-07 11:15:38 -0400

    Add operate function
    The operate function takes two numbers and an operator string.
    The function then returns the result of that operator on the two
    numbers provided.

--- javascript.js
+++ javascript.js
@@ -1,3 +1,7 @@
+let num1 = null;
+let num2 = null;
+let operator = null;
+
 const add = function(num1, num2) {
     return num1 + num2;
 }
@@ -12,4 +16,25 @@ const multiply = function(num1, num2) {
 
 const divide = function(num1, num2) {
     return num1 / num2;
+}
+
+const operate = function(num1, num2, operator) {
+    let result = null;
+
+    switch (operator) {
+        case "add":
+            result = add(num1, num2);
+            break;
+        case "subtract":
+            result = subtract(num1, num2);
+            break;
+        case "multiply":
+            result = multiply(num1, num2);
+            break;
+        case "divide":
+            result = divide(num1, num2);
+            break;
+    }
+
+    return result;
 }
